a803ac89b31a3f11669f6fa7a1d6200495eed7c6
VictorBjorke <144603139+VictorBjorke@users.noreply.github.com>
2023-09-16 15:47:04 +0200

    Update config-gitignore.sh
    Improved error handling, made it delete duplicate entries to the gitignore file.

--- config-gitignore.sh
+++ config-gitignore.sh
@@ -1,13 +1,36 @@
 #!/bin/bash
 
-# Enable verbose output
-set -x
+# Enable error handling
+set -e
 
-# Add .DS_Store to ~/.gitignore_global
-echo ".DS_Store" >> ~/.gitignore_global
+# Check if Git is installed
+if ! [ -x "$(command -v git)" ]; then
+  echo "Git is not installed. Please install Git and run this script again."
+  exit 1
+fi
 
-# Configure git to use ~/.gitignore_global
-git config --global core.excludesfile ~/.gitignore_global
+# Define the path to .gitignore_global
+global_gitignore=~/.gitignore_global
 
-# Disable verbose output
-set +x
+# Check if .gitignore_global already contains the entry
+if grep -qF ".DS_Store" "$global_gitignore"; then
+  # Count the number of occurrences of .DS_Store in .gitignore_global
+  count=$(grep -c -F ".DS_Store" "$global_gitignore")
+
+  if [ "$count" -gt 1 ]; then
+    # Remove all but the first occurrence of .DS_Store
+    sed -i '' '2,$ {/\.DS_Store/d;}' "$global_gitignore"
+    echo "Removed duplicate .DS_Store entries from $global_gitignore."
+  else
+    echo ".DS_Store is already in $global_gitignore. No changes were made."
+  fi
+else
+  # Add .DS_Store to ~/.gitignore_global
+  echo ".DS_Store" >> "$global_gitignore"
+  # Configure git to use ~/.gitignore_global
+  git config --global core.excludesfile "$global_gitignore"
+  echo "Git is now configured to ignore .DS_Store files globally."
+fi
+
+# Disable error handling
+set +e
